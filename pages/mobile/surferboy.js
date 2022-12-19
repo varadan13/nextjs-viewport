f86df925ed73767d83b4499b96e8bf7a1cad1786
varadan13 <rcaesar1996@gmail.com>
2022-12-19 13:52:36 +0530

    lazy commit

--- pages/mobile/surferboy.js
+++ pages/mobile/surferboy.js
@@ -1,4 +1,4 @@
-import { chunk, differenceWith } from "lodash";
+import { chunk, differenceWith, isEqual } from "lodash";
 
 const Index = () => {
   chunk(["a", "b", "c", "d"], 2);
@@ -7,7 +7,7 @@ const Index = () => {
     { x: 2, y: 1 },
   ];
 
-  differenceWith(objects, [{ x: 1, y: 2 }], _.isEqual);
+  differenceWith(objects, [{ x: 1, y: 2 }], isEqual);
   // => [{ 'x': 2, 'y': 1 }]
   return <div>surferboy</div>;
 };
