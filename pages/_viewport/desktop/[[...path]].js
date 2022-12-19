b080aef4d1f3ca17b4bca4cd09173a5fb6caffda
varadan13 <rcaesar1996@gmail.com>
2022-12-19 10:35:39 +0530

    lazy commit

--- pages/_viewport/desktop/[[...path]].js
+++ pages/_viewport/desktop/[[...path]].js
@@ -1,11 +1,14 @@
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
+import * as R from "ramda";
+
+const { identity } = R;
 
 export default function Desktop() {
-  const router = useRouter()
-
+  const router = useRouter();
+  console.log(R.map(identity, [1, 2, 3]));
   if (router.isReady) {
-    console.log(router)
+    console.log(router);
   }
 
-  return "This loads on desktop"
+  return "This loads on desktop";
 }
--- pages/_viewport/mobile/[[...path]].js
+++ pages/_viewport/mobile/[[...path]].js
@@ -1,11 +1,12 @@
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
+import { chunk } from "lodash";
 
 export default function Mobile() {
-  const router = useRouter()
-
+  const router = useRouter();
+  console.log(chunk(["a", "b", "c", "d"], 2));
   if (router.isReady) {
-    console.log(router)
+    console.log(router);
   }
 
-  return "This loads on mobile"
+  return "This loads on mobile";
 }
