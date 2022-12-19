495742f8508cd940ab5d78eee546384e1fbc08e7
varadan13 <rcaesar1996@gmail.com>
2022-12-19 15:34:57 +0530

    upd

--- pages/desktop/surferboy.js
+++ pages/desktop/surferboy.js
@@ -11,9 +11,6 @@ const Index = () => {
   mapIndexed((val, idx) => idx + "-" + val, ["f", "o", "o", "b", "a", "r"]);
   //=> ['0-f', '1-o', '2-o', '3-b', '4-a', '5-r']
 
-  R.adjust(1, R.toUpper, ["a", "b", "c", "d"]); //=> ['a', 'B', 'c', 'd']
-  R.adjust(-1, R.toUpper, ["a", "b", "c", "d"]); //=> ['a', 'b', 'c', 'D']
-
   const equals3 = R.equals(3);
   R.all(equals3)([3, 3, 3, 3]); //=> true
   R.all(equals3)([3, 3, 1, 3]); //=> false
