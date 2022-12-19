67d748e7778608e852dc83ef1016b27760ab2b03
varadan13 <rcaesar1996@gmail.com>
2022-12-19 14:07:17 +0530

    lazy loading

--- pages/desktop/newboy.js
+++ pages/desktop/newboy.js
@@ -1,5 +1,7 @@
+import Link from "next/link";
+
 const Index = () => {
-  return <a href="./surferboy">Surferboy</a>;
+  return <Link href="./surferboy">Surferboy</Link>;
 };
 
 export default Index;
